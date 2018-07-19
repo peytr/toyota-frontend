812338afd2ed747d3bcc903f0a5838ae7b0d88fc
Natalie Tan <natalieytan@gmail.com>
2018-07-19 11:27:14 +1000

    Place backend API in .env

--- src/components/api/init.js
+++ src/components/api/init.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://toyota-backend-rtodfuwkmw.now.sh/api',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   withCredentials: true
 })
 
