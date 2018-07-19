d64d697de163f545b5357b0e8b9de34a1b67399b
peytr <peytr@mail.com>
2018-07-19 19:48:35 +1000

    Add AdminViewUser using LC Registration Form code.

--- src/components/user/AdminViewUser.js
+++ src/components/user/AdminViewUser.js
@@ -1,114 +1,125 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import axios from '../api/init'
 import instance from '../api/init'
 
 class AdminViewUser extends Component {
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super()
     this.state = {
       user: null
     }
-    // bind handle form change to this class
-    this.handleChange = this.handleChange.bind(this);
-    // bind handle submit 
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handle change function executed on form input change
-  // sets the state to the current value of the input
-  handleChange (event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-        [name]: value
-    });
-  }
-
-  // handle submit executes on form submit
-  // logs current value of inputs
-  // prevents default form action
-  handleSubmit (event) {
-    console.log('Form value: ' + this.state.inputvalue);
-    event.preventDefault();
-  }
-
-  componentDidMount() {
-    instance.get('/users/5b4c221a7c75af5f989a3e35')
-     .then((response) => {
-       console.log(response);
-       this.setState({user: response.data});
-     })
-    .catch((error)=>{
-       console.log(error);
-    })
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value})
   }
   
-  render() {
-    if(this.state.user) {
-    return (
-      
-      <div>
-        <h1>Admin View User</h1>
+  onSubmit(e) {
+    e.preventDefault()
+    const user = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      employeeNumber: this.state.employeeNumber,
+      email: this.state.email,
+      department: this.state.department,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(user)
+    axios.post("/users/5b4c221a7c75af5f989a3e35", user) 
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    .catch(err => console.error(err))
+  }
 
-        <div className="field">
-            <label className="label">First Name</label>
-            <div className="control">
-                <input className="input" type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder={this.state.user.firstName}/>
-            </div>
-            <p></p>
-            <label className="label">Last Name</label>
-            <div className="control">
-                <input className="input" type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder={this.state.user.lastName}/>
-            </div>
-            <p></p>
-            <label className="label">Employee Number</label>
-            <div className="control">
-                <input className="input" type="text" name="employeeNumber" value={this.state.employeeNumber} onChange={this.handleChange} placeholder={this.state.user.employeeNumber}/>
-            </div>
-            <p></p>
-            <label className="label">Email</label>
-            <div className="control">
-                <input className="input" type="text" name="email" value={this.state.email} onChange={this.handleChange} placeholder={this.state.user.email}/>
-            </div>
-            <p></p>
-            <label className="label">Department</label>
-            <div className="control">
-                <input className="input" type="text" name="department" value={this.state.department} onChange={this.handleChange} placeholder={this.state.user.department}/>
-            </div>
-            <p></p>
-            <div className="control">
-              <label className="checkbox">
-                <input
-                  name="active"
-                  type="checkbox"
-                  checked={this.state.active}
-                  onChange={this.handleChange}
+componentDidMount() {
+  instance.get('/users/5b4c221a7c75af5f989a3e35')
+   .then((response) => {
+     console.log(response);
+     this.setState({user: response.data});
+   })
+  .catch((error)=>{
+     console.log(error);
+  })
+}
+
+  
+
+  render() {
+    return (
+      <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+          <br/>
+            <p className="lead text-center">Update User</p>
+           
+            <form onSubmit={this.onSubmit.bind(this)}>
+           
+            <br/>
+              <div className="form-group">
+                <input type="text" 
+                className="form-control 
+                form-control-md" 
+                placeholder='First Name'
+                name="firstName" 
+                value={this.state.firstName}
+                onChange={this.onChange.bind(this)}
                 />
-                Active
-              </label>
-            </div>
-            <p></p>
-            <div className="control">
-              <label className="checkbox">
-                <input
-                  name="admin"
-                  type="checkbox"
-                  checked={this.state.admin}
-                  onChange={this.handleChange}
+              </div>
+              <div className="form-group">
+                <input 
+                type="text" 
+                className="form-control form-control-md" 
+                placeholder="Last Name" 
+                name="lastName" 
+                value={this.state.lastName}
+                onChange={this.onChange.bind(this)} 
                 />
-                Admin
-              </label>
+              </div>
+              <div className="form-group">
+                <input 
+                type="text" 
+                className="form-control form-control-md" 
+                placeholder="Toyota Number" 
+                name="employeeNumber"
+                value={this.state.employeeNumber}
+                onChange={this.onChange.bind(this)}
+                 />
+              </div>
+              <div className="form-group">
+                <input 
+                type="email" 
+                className="form-control 
+                form-control-md" 
+                placeholder="Email" 
+                name="email" 
+                value={this.state.email}
+                onChange={this.onChange.bind(this)}
+                />
+              </div>
+              <div className="form-group">
+                <select className="form-control" name="department" value={this.state.department} onChange={this.onChange.bind(this)}>
+                  <option>Product Planning & Pricing</option>
+                  <option>Product Design</option>
+                  <option>Regulations, Conversions & Accessories</option>
+                  <option>Vehicle Evaluation</option>
+                  <option>Connected Vehicle Services</option>
+                  <option>Technical administration</option>
+                </select>
+              </div>
+              <br/>
+              <div className="text-center"> 
+              <input type="submit" className="btn btn-primary" />
             </div>
-            <input type="submit" value="Submit"/>
+            </form>
+          </div>
         </div>
       </div>
-    )
-    } 
-    return (
-      <p>Loading user</p>
+    </div>
+    
     )
   }
 }
