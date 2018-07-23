f12224b6ab3c1aa539148860f18cda6f4d9e4eac
Natalie Tan <natalieytan@gmail.com>
2018-07-24 02:17:31 +1000

    Add ReactTable to ManageUsers

--- src/components/user/ManageUsers.js
+++ src/components/user/ManageUsers.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import instance from '../api/init'
 import ListWrapper from './ListWrapper.js'
+import ReactTable from 'react-table'
 
 class ManageUsers extends Component {
-
   state = {users: [{}] } 
-
   componentDidMount() {
     instance.get('users/')
      .then((response) => {
@@ -21,13 +20,59 @@ class ManageUsers extends Component {
     })
   }
 
-
   render() {
     return (
       <div>
-        <h1>Manage Users </h1>
-        <ListWrapper {...this.state} />
-        <Link to="/register"><button className="btn btn-secondary">Create New User</button></Link>
+        <div className="solid-heading  d-flex justify-content-between">Manage Users <Link to="/register"><button className="btn btn-secondary">Create New User</button></Link></div>
+        <ReactTable 
+          data={this.state.users}
+          columns={[
+            {
+              Header: "First Name",
+              accessor: "firstName",
+            },
+            {
+              Header: "Last Name",
+              accessor: "lastName"
+            },
+            {
+              Header: "Employee Number",
+              accessor: "employeeNumber"
+            },
+            {
+              Header: "Email",
+              accessor: "email"
+            },
+            {
+              Header: "Department",
+              accessor: "department"
+            },
+            {
+              id: "Admin",
+              Header: "Admin",
+              accessor: d => {
+                return d.administrator ? "Yes" : "No"
+              }
+            },
+            {
+              id: "Active",
+              Header: "Active",
+              accessor: d => {
+                return d.active ? "Yes" : "No"
+              }
+            },            
+            {
+              Header: "Edit",
+              accessor: "_id",
+              Cell: row => (
+                <Link to={`/users/${row.value}`}>Edit</Link>
+              )
+            }
+          ]}
+          className="-striped -highlight"
+          minRows={1}
+          filterable={true}
+        />
       </div>
     )
   }
